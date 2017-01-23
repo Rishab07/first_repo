01c8dc4faecd0e7fc582b73a3133e8a1e04b5351
iamrishab07 <smrishabnxt001@gmail.com>
2017-01-23 16:29:25 +0530

    Simple program to double a number

--- testProgram.cpp
+++ testProgram.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main(){
-	cout<<"This is a new branch.";
+	//Program to double up a number
+	int n;
+	cout<<"Enter a number:\n";
+	cin>>n;
+	cout<<"Here's the double value:   "<<(2*n);
 	return 0;
 }
